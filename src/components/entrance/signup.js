1fa154b8c01f6982af86015fcb3397cd5de575db
charliefernandis <charliefernandis28@gmail.com>
2023-12-26 14:44:28 +0530

    26-12-2023 commit-1

--- src/components/entrance/signup.js
+++ src/components/entrance/signup.js
@@ -6,69 +6,77 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import { NavLink } from "react-router-dom";
 
 
-const SignUp = ()=>{
+const SignUp = () => {
 
-    const [logData , setLogData] = useState({
-        email:"",
-        username:"",
-        password:"",
-        cpassword:""
+    const [logData, setLogData] = useState({
+        email: "",
+        username: "",
+        password: "",
+        cpassword: ""
     })
 
-    const handleSubmit = async(e)=>{
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        const {email , username , password , cpassword} = logData;
-        if(!email || !username || !password || !cpassword){
+        const { email, username, password, cpassword } = logData;
+        if (!email || !username || !password || !cpassword) {
             alert("fill all the details first");
         }
-        else{
-            if(password == cpassword){
-                const res = await fetch("/skillapp/signup" , {
-                    method:"POST",
-                    headers:{
-                        "content-type":"application/json"
-                    },
-                    body:JSON.stringify({email:email ,username:username , password:password} )
-                })
-
-                const data = await res.json();
-                console.log(data);
-                if(data.error){
-                    alert(data.error);
+        else {
+            if (password == cpassword) {
+                try {
+                    const res = await fetch("https://mobilehub-0054a.onrender.com/skillapp/signup", {
+                        method: "POST",
+                        headers: {
+                            "content-type": "application/json"
+                        },
+                        body: JSON.stringify({ email , username , password})
+                    })
+                    const data = await res.json();
+                    console.log(data);
+                    if (data.error) {
+                        alert(data.error);
+                    }
                 }
+                catch (error) {
+                    console.log("error: ", error);
+                }
+
+
             }
-            else{
+            else {
                 alert("password and confirm password does not match");
             }
         }
     }
 
-    const addData = (e)=>{
-        const {name , value} = e.target
+    const addData = (e) => {
+        const { name, value } = e.target
         // console.log(name , value);
-        setLogData(()=>{
-           return {
+        setLogData(() => {
+            return {
                 ...logData,
-                [name]:value
+                [name]: value
             }
         })
     }
 
 
 
-    return  <div style={{height:"100vh"}} className="loginform-container">
-    <div className="login-form">
-        <input type="text" value={logData.email} onChange={addData} required name="email" placeholder="Enter Email " />
-        <input type="text" value={logData.username} onChange={addData}  name="username" placeholder="Enter Username " />
-        <input type="password" value={logData.password} onChange={addData}  name="password" placeholder="password" />
-        <input type="password" value={logData.cpassword} onChange={addData} name="cpassword" placeholder="confirm password" />
-        <button style={{margin:"20px  0 30px 0"}}  onClick={handleSubmit} className="signup-button submit-button">Sign Up &rarr;</button>
-        <span style={{color:"rgb(84, 82, 82)" , fontSize:"70%" , margin:"-0px 0 10px 0" }}>Already have an account? Click here to <NavLink to="/login">Login</NavLink>. </span>
-        <button className="login-with-google"><GoogleIcon /><span>Continue With Google</span></button>
-        <button className="login-with-github"><GitHubIcon /><span>Continue With Github</span></button>
-        <button className="login-with-facebook"><FacebookIcon /><span>Continue With Facebook</span></button>
+    return <div style={{ height: "100vh" }} className="loginform-container">
+        <div className="login-form">
+            <form method="POST" action="">
+                <input type="text" value={logData.email} onChange={addData} required name="email" placeholder="Enter Email " />
+                <input type="text" value={logData.username} onChange={addData} name="username" placeholder="Enter Username " />
+                <input type="password" value={logData.password} onChange={addData} name="password" placeholder="password" />
+                <input type="password" value={logData.cpassword} onChange={addData} name="cpassword" placeholder="confirm password" />
+                <button style={{ margin: "20px  0 30px 0" }} onClick={handleSubmit} className="signup-button submit-button">Sign Up &rarr;</button>
+            </form>
+            <span style={{ color: "rgb(84, 82, 82)", fontSize: "70%", margin: "-0px 0 10px 0" }}>Already have an account? Click here to <NavLink to="/login">Login</NavLink>. </span>
+            <button className="login-with-google"><GoogleIcon /><span>Continue With Google</span></button>
+            <button className="login-with-github"><GitHubIcon /><span>Continue With Github</span></button>
+            <button className="login-with-facebook"><FacebookIcon /><span>Continue With Facebook</span></button>
+        </div>
     </div>
-</div>
 }
 
 export default SignUp;
